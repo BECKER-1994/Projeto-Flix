247c73d545d4fbb3eaef9de5e6231bb41952b8fe
BECKER-1994 <anaps_becker@hotmail.com>
2021-07-24 22:19:06 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+function comecar(){
+
+        window.open("https://www.amazon.com.br/Senhor-dos-An%C3%A9is-Sociedade-Anel/dp/8595084750/ref=sr_1_2?dchild=1&keywords=o+senhor+dos+aneis&qid=1626638249&s=books&sr=1-2"); //abre nova guia
+       // window.location.href = "" //mesma pagina
+}
+
+function informacoes(){
+    window.open("https://www.amazon.com.br/Senhor-dos-An%C3%A9is-Sociedade-Anel/dp/8595084750/ref=sr_1_2?dchild=1&keywords=o+senhor+dos+aneis&qid=1626638249&s=books&sr=1-2");
+}
